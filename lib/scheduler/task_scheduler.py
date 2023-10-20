ad4c00213b03e33ee909e098143bae8582fe9971
messiah1349 <messiah1349@gmail.com>
2023-10-20 21:03:06 +0300

    aws preparing

--- lib/scheduler/task_scheduler.py
+++ lib/scheduler/task_scheduler.py
@@ -1,15 +1,17 @@
 from datetime import datetime
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.jobstores.base import JobLookupError
 from abc import ABC, abstractmethod
 import logging
 
 logger = logging.getLogger(__name__)
 
+
 class TaskScheduler(ABC):
 
     def __init__(self):
         self.schedule = AsyncIOScheduler(daemon=True)
-        
+
     def __repr__(self) -> str:
         return str(self.schedule)
 
@@ -18,26 +20,28 @@ class TaskScheduler(ABC):
         raise NotImplementedError
 
     def add_task(self, task_id: str, user_id: int, task_name: str, task_time: datetime):
-        
+
         self.schedule.add_job(
-            self.send_message, 
-            'date', 
-            run_date=task_time, 
+            self.send_message,
+            'date',
+            run_date=task_time,
             kwargs={
                 "user_id": user_id,
                 "message_text": task_name,
                 "deed_id": task_id
             },
-            id=str(task_id), 
+            id=str(task_id),
             replace_existing=True
         )
         logger.debug(f"added job {task_name=} with {task_id=} at {task_time=}")
 
     def delete_task(self, task_id: str):
-        self.schedule.remove_job(task_id)
-        logger.debug(f"removed job {task_id=}")
+        try:
+            self.schedule.remove_job(task_id)
+            logger.debug(f"removed job {task_id=}")
+        except JobLookupError:
+            logger.info(f"{task_id=} recieved but is not in scheduler")
 
     def start_scheduling(self):
         self.schedule.start()
         logger.info("scheduler started")
-
